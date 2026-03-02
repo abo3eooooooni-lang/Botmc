44b7419d974989b4be1591e773a6748585e4b80e
abo3eooooooni-lang <abo3eooooooni@gmail.com>
2026-03-02 12:25:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function startBot() {
     host: 'yyyycraft.falixsrv.me',
     username: 'shay', // اسم مختلف عن حسابك الأساسي
     auth: 'offline',          // cracked
-    version: '1.20.4'
+    version: '1.21.11'
   });
 
   bot.on('spawn', () => {
