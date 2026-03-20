1dda08d0cc2f4039713b6142269cbde796ecc94c
abo3eooooooni-lang <abo3eooooooni@gmail.com>
2026-03-21 00:23:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function createBot(options) {
 
     if (!registered) {
       setTimeout(() => {
-        bot.chat('/register 123yyyuuu 123yyyuuu');
+        bot.chat('/reg 123yyyuuu');
         registered = true;
       }, 2000);
     }
